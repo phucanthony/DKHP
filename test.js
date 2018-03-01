5e06a0b6c55abd81c2fc65e15bef4039e585ac9c
PhucDesktop <phuc9h2010@gmail.com>
2018-03-01 12:11:14 +0700

    initial commit

--- test.js
+++ test.js
@@ -4,9 +4,9 @@ const webDriver = require('selenium-webdriver');
 
 	const driver = new webDriver.Builder().withCapabilities(webDriver.Capabilities.chrome()).build();
 	await driver.get('https://dkhp.hcmue.edu.vn/');
-	await checkLogin(driver);
+	// await checkLogin(driver);
 	// const register = await setInterval(() => registerListening6(driver), 1000);
-	await registerListening6(driver, true, false);
+	await registerListening6(driver, true, true);
 	//driver.quit();
 })();
 
@@ -21,7 +21,7 @@ async function checkLogin(driver) {
 		await console.log(url);
 		if (url.indexOf('Login') !== -1) {
 			await driver.findElement(webDriver.By.name('username')).sendKeys('41.01.701.122');
-			await driver.findElement(webDriver.By.name('password')).sendKeys('Thutrang.');
+			await driver.findElement(webDriver.By.name('password')).sendKeys('Chaulen3');
 			await driver.findElement(webDriver.By.className('loginbox-submit')).click();
 			await driver.sleep(1000);
 			await driver.executeScript('window.scrollBy(0, 200)','');
@@ -29,6 +29,11 @@ async function checkLogin(driver) {
 			await driver.findElement(webDriver.By.id('DangKyHocPhan&KH')).click();
 			await driver.sleep(100);
 		}
+		await driver.sleep(2000);
+		await driver.executeScript('window.scrollBy(0, 600)','');
+		await driver.sleep(100);
+		await driver.findElement(webDriver.By.css('a[href*="javascript:GetClassStudyUnit(\'1721ENGL1416\',\'Biên dịch thực hành')).click();
+
 		await resolve();
 	});
 }
@@ -39,26 +44,20 @@ async function registerListening6(driver, isRepeat = false, isSecondTime = false
 		if (isSecondTime) {
 			await driver.sleep(1000);
 			const tableList = await driver.findElements(webDriver.By.css("td"));
-			const limit = await tableList[54].getText();
-			const currentStudent = await tableList[55].getText();
+			const limitClassOne = await tableList[63].getText();
+			const currentStudentClassOne = await tableList[64].getText();
+			const limitClassTwo = await tableList[72].getText();
+			const currentStudentClassTwo = await tableList[73].getText();
 			if (parseInt(limit.substr(limit.indexOf('-') + 1)) <= parseInt(currentStudent)) {
 				console.log('full', parseInt(limit.substr(limit.indexOf('-') + 1)) , parseInt(currentStudent));
-				if (isRepeat) {
-					await driver.executeScript("location.reload()");
-					await driver.sleep(100);
-					await registerListening6(driver, true, false);
-				}
 			} else {
+				console.log('not full', limit.substr(limit.indexOf('-') + 1) , currentStudent);
 				await driver.findElement(webDriver.By.id('1721ENGL141601$0.0$1721ENGL1416$$0')).click();
 				const buttonList = await driver.findElements(webDriver.By.css("input"));
 				await buttonList[5].click();
 				await driver.quit();
 			}
 		}
-		await driver.sleep(2000);
-		await driver.executeScript('window.scrollBy(0, 600)','');
-		await driver.sleep(100);
-		await driver.findElement(webDriver.By.css('a[href*="javascript:GetClassStudyUnit(\'1721ENGL1416\',\'Biên dịch thực hành')).click();
 		await registerListening6(driver, true, true);
 
 		await resolve();
